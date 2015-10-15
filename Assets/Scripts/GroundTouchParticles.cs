746c3d9f4b2090c80446ad6e80a5350b0b74bae2
41407 <jimi.welling@cs.helsinki.fi>
2015-10-15 14:53:23 +0300

    Add particle effects

---
+++ Assets/Scripts/GroundTouchParticles.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundTouchParticles : MonoBehaviour
+{
+	public GameObject groundParticle;
+	public Vector2 offset;
+
+	void Awake ()
+	{
+		groundParticle = (GameObject)Resources.Load ("Ground Particle");
+	}
+	
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if (Mathf.Abs(col.relativeVelocity.y) > 1) {
+			GameObject newParticle = (GameObject)Instantiate (groundParticle, (Vector2)transform.position + offset, Quaternion.identity);
+			newParticle.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>().startSize = Mathf.Abs(col.relativeVelocity.y) / 90f;
+			newParticle.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>().startSpeed = Mathf.Abs(col.relativeVelocity.y);
+		}
+	}
+}
