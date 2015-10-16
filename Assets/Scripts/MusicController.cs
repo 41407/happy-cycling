f18284d19f9f57232e08aa04b3608c902911b22f
41407 <jimi.welling@cs.helsinki.fi>
2015-10-16 17:31:41 +0300

    Add m for music toggle

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -4,11 +4,19 @@ using System.Collections;
 public class MusicController : MonoBehaviour
 {
 	private AudioSource aud;
+	public bool musicOn = true;
 
 	void Awake ()
 	{
 		aud = GetComponent<AudioSource> ();
 		DontDestroyOnLoad (this.gameObject);
 		aud.Play ();
+	}
+
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.M)) {
+			aud.volume = (aud.volume > 0) ? 0 : 0.7f;
+		}
 	}
 }
