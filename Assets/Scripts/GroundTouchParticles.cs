277171f110c8170d25a76de71b4a5181e4b56503
41407 <jimi.welling@cs.helsinki.fi>
2015-10-17 17:32:36 +0300

    Better particles

--- Assets/Scripts/GroundTouchParticles.cs
+++ Assets/Scripts/GroundTouchParticles.cs
@@ -18,14 +18,15 @@ public class GroundTouchParticles : MonoBehaviour
 	
 	void OnCollisionEnter2D (Collision2D col)
 	{
-		if (Mathf.Abs (col.relativeVelocity.y) > particleThreshold) {
+		if (Mathf.Abs (col.relativeVelocity.magnitude) > particleThreshold) {
 			if (Mathf.Abs (col.relativeVelocity.y) > soundThreshold) {
 				aud.volume = Mathf.Abs (col.relativeVelocity.y) / 10;
 				aud.PlayOneShot (groundHit);
 			}
-			GameObject newParticle = (GameObject)Instantiate (groundParticle, (Vector2)transform.position + offset, Quaternion.identity);
-			newParticle.transform.GetChild (0).gameObject.GetComponent<ParticleSystem> ().startSize = Mathf.Abs (col.relativeVelocity.y) / 90f;
-			newParticle.transform.GetChild (0).gameObject.GetComponent<ParticleSystem> ().startSpeed = Mathf.Abs (col.relativeVelocity.y);
+			GameObject newParticle = (GameObject)Instantiate (groundParticle, col.contacts[0].point, Quaternion.LookRotation(-col.contacts[0].normal));
+			ParticleSystem particles = newParticle.transform.GetChild (0).gameObject.GetComponent<ParticleSystem> ();
+			particles.startSize = Mathf.Clamp (Mathf.Abs (col.relativeVelocity.y) / 90f, 0.01f, 0.08f);
+			particles.startSpeed = Mathf.Clamp (Mathf.Abs (col.relativeVelocity.y), 1, 8);
 			Destroy (newParticle, 5);
 		}
 	}
