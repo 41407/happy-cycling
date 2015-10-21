725e49a82c4bc34d7d9517aa9f5c317a0035bd89
41407 <jimi.welling@cs.helsinki.fi>
2015-10-21 17:33:11 +0300

    Main menu screen, tutorial cave

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -1,20 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SceneLoader : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
 	public string sceneName;
 	private AsyncOperation async;
+	public float timeBeforeStart = 1;
+	public int state = 0;
 
 	void Start ()
 	{
 		DontDestroyOnLoad (gameObject);
 		PlayerPrefs.SetInt ("Level", 0);
+	}
+
+	void LoadGame ()
+	{
 		async = Application.LoadLevelAsync (sceneName);
+	}
+
+	void Advance ()
+	{
+		state++;
 	}
 
 	void Update ()
 	{
+		if (Input.anyKeyDown && state == 2) {
+			LoadGame ();
+		}
 		if (async != null) {
 			if (async.isDone) {
 				GameObject.Find ("Music").SendMessage ("Play");
