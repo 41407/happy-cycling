be546c5e3b932551d538f3574eb653076049ed35
41407 <jimi.welling@cs.helsinki.fi>
2015-10-21 23:57:29 +0300

    Ending logic

---
+++ Assets/Scripts/Levels and Data/TheEnd.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheEnd : MonoBehaviour
+{
+
+	void Start ()
+	{
+		if (TimeRecord ()) {
+			PlayerPrefs.SetFloat ("TimeRecord", Score.GetTime ());
+		}
+		if (CrashesRecord ()) {
+			PlayerPrefs.SetInt ("CrashesRecord", Score.GetCrashes ());
+		}
+	}
+
+	void Update ()
+	{
+		if (Input.anyKeyDown) {
+			Application.LoadLevelAsync ("Main Menu");
+		}
+	}
+
+	bool TimeRecord ()
+	{
+		return PlayerPrefs.GetFloat ("TimeRecord") > Score.GetTime ();
+	}
+
+	bool CrashesRecord ()
+	{
+		return PlayerPrefs.GetInt ("CrashesRecord") > Score.GetCrashes ();
+	}
+}
