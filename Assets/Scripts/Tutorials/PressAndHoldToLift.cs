4de48dc18059ad0a35b1706af075b746eb2aacef
41407 <jimi.welling@cs.helsinki.fi>
2015-10-24 22:07:07 +0300

    Make first tutorial interactive

---
+++ Assets/Scripts/Tutorials/PressAndHoldToLift.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressAndHoldToLift : MonoBehaviour
+{
+	public Transform player;
+	public SceneController sc;
+	public bool tutorialTriggered = false;
+
+	void Start ()
+	{
+		print (player);
+		sc = GameObject.Find ("Scene Controller").GetComponent<SceneController> ();
+	}
+
+	void Update ()
+	{
+		if (!player) {
+			player = GameObject.FindGameObjectWithTag ("Player").transform;
+
+		}
+		if (player.position.x > transform.position.x && tutorialTriggered == false) {
+			sc.SendMessage ("SetPaused", true);
+			tutorialTriggered = true;
+			transform.GetChild (0).gameObject.SetActive (true);
+		}
+		if (tutorialTriggered == true && (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0))) {
+			sc.SendMessage ("SetPaused", false);
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/UI/SpriteColorFlash.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteColorFlash : MonoBehaviour
+{
+	public Color a;
+	public Color b;
+	private SpriteRenderer sr;
+	public int frameRate = 2;
+
+	void OnEnable ()
+	{
+		sr = GetComponent<SpriteRenderer> ();
+		sr.color = a;
+	}
+
+	void Update ()
+	{
+		if (Time.frameCount % frameRate == 0) {
+			SwapColors ();
+		}
+	}
+
+	void SwapColors ()
+	{
+		if (sr.color.Equals (a)) {
+			sr.color = b;
+		} else {
+			sr.color = a;
+		}
+	}
+}
