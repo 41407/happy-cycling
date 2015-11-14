5a828be42cc4b36c8dbcf42e39c0d68b153bbbbf
41407 <jimi.welling@cs.helsinki.fi>
2015-11-14 22:47:42 +0200

    Animate cat's tail

---
+++ Assets/Scripts/Menu/MenuCatLogic.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuCatLogic : MonoBehaviour
+{
+	public float probability = 0.1f;
+
+	void Start ()
+	{
+		if (Random.value > probability) {
+			Destroy (gameObject);
+		}
+	}
+}
