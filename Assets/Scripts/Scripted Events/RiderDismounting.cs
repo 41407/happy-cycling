d4fd2e40704b2d606b74499922e398a04fce97ed
41407 <jimi.welling@cs.helsinki.fi>
2015-11-21 23:56:01 +0200

    Rider walking into the house

--- Assets/Scripts/Scripted Events/RiderDismounting.cs
+++ Assets/Scripts/Scripted Events/RiderDismounting.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class RiderDismounting : MonoBehaviour
 {
-
+	
 	public float helmetThrowDelay = 0.5f;
+	public float walkDelay = 2.0f;
+	public float walkSpeed = 1.0f;
 	public GameObject helmetPrefab;
+	public bool walking = false;
 
 	void Start ()
 	{
@@ -15,5 +18,19 @@ public class RiderDismounting : MonoBehaviour
 	void HelmetThrow ()
 	{
 		Instantiate (helmetPrefab, transform.position + Vector3.up / 2, Quaternion.identity);
+		Invoke ("Walk", helmetThrowDelay);
+	}
+
+	void Walk ()
+	{
+		GetComponent<Animator> ().SetBool ("Walk", true);
+		walking = true;
+	}
+
+	void Update ()
+	{
+		if (walking) {
+			transform.Translate (Vector2.right * walkSpeed * Time.deltaTime);
+		}
 	}
 }
