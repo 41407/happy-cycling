41ca2c649915ba713422897f09c83ffa362f630b
41407 <jimi.welling@cs.helsinki.fi>
2015-11-22 22:07:06 +0200

    Sounds for splash logo, heart, new main menu music

---
+++ Assets/Scripts/Audio/PlayOnStart.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayOnStart : MonoBehaviour {
+
+	private AudioSource aud;
+	public AudioClip clip;
+	public float volume = 1;
+	public float delay = 0.25f;
+
+	void Awake () {
+		aud = GetComponent<AudioSource> ();
+		Invoke ("Play", delay);
+	}
+
+	void Play () {
+		aud.PlayOneShot(clip, volume);
+	}
+}
