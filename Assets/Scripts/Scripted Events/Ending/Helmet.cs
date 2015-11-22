0089b65cb60b225eb1cbdb3c9ae0822305c92b9f
41407 <jimi.welling@cs.helsinki.fi>
2015-11-22 16:33:29 +0200

    More sounds for ending cutscene

--- Assets/Scripts/Scripted Events/Ending/Helmet.cs
+++ Assets/Scripts/Scripted Events/Ending/Helmet.cs
@@ -1,10 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Helmet : MonoBehaviour {
+public class Helmet : MonoBehaviour
+{
 
-	void Start () {
-		GetComponent<Rigidbody2D>().AddForce((Vector2.right + Vector2.up) * 300);
-		GetComponent<Rigidbody2D>().AddTorque(10);
+	private Rigidbody2D body;
+	private AudioSource aud;
+	public AudioClip hit;
+
+	void Awake ()
+	{
+		body = GetComponent<Rigidbody2D> ();
+		aud = GetComponent<AudioSource> ();
+	}
+
+	void Start ()
+	{
+		body.AddForce ((Vector2.right + Vector2.up) * 300);
+		body.AddTorque (10);
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		aud.PlayOneShot (hit, Mathf.Clamp (col.relativeVelocity.magnitude / 2f, 0, 1));
 	}
 }
