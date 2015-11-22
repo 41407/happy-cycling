29341883b6556fc0e2097fabe166a28770a9bf67
41407 <jimi.welling@cs.helsinki.fi>
2015-11-22 23:57:27 +0200

    Fix keyboard input lag

--- Assets/Scripts/Input/PlayerInput.cs
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -5,26 +5,24 @@ public class PlayerInput : MonoBehaviour
 {
 	private bool paused = false;
 	private bool mouseControlled = true;
+	private InputMethod previousInput;
+
+	private enum InputMethod
+	{
+		mouse,
+		button
+	}
 
 	void Update ()
 	{
 		if (!paused) {
 			if (KeyDown ()) {
+				CheckIfInputMethodChanged ();
 				SendMessage ("Pump");
 			}
 			if (KeyUp ()) {
 				SendMessage ("Go");
 				SendMessage ("Jump");
-			}
-			if ((mouseControlled && Input.GetButton ("Jump")) || (!mouseControlled && Input.GetMouseButton (0))) {
-				mouseControlled = !mouseControlled;
-				if (!mouseControlled) {
-					UnityEngine.Cursor.visible = false;
-				} else {
-					UnityEngine.Cursor.visible = true;
-				}
-				paused = true;
-				Invoke ("Continue", 0.5f);
 			}
 		}
 	}
@@ -36,12 +34,24 @@ public class PlayerInput : MonoBehaviour
 	
 	private bool KeyUp ()
 	{
+
 		return Input.GetMouseButtonUp (0) || Input.GetButtonUp ("Jump");
 	}
 	
 	private bool Key ()
 	{
 		return Input.GetMouseButton (0) || Input.GetButton ("Jump");
+	}
+
+	void CheckIfInputMethodChanged ()
+	{
+		InputMethod current = Input.GetButtonDown ("Jump") ? InputMethod.button : InputMethod.mouse;
+		if (previousInput != null && previousInput != current) {
+			paused = true;
+			Invoke ("Continue", 0.5f);
+			UnityEngine.Cursor.visible = Input.GetMouseButtonDown (0);
+			previousInput = current;
+		}
 	}
 
 	void Pause ()
