a905d613c7dcc02f28f04dea64caf651501fdadb
41407 <jimi.welling@cs.helsinki.fi>
2016-03-06 22:31:41 +0200

    Fix menu scenes to avoid multiple LoadSceneAsync calls

--- Assets/Scripts/Menu/LoadSavedGameMenuController.cs
+++ Assets/Scripts/Menu/LoadSavedGameMenuController.cs
@@ -4,31 +4,42 @@ using UnityEngine.SceneManagement;
 
 public class LoadSavedGameMenuController : MonoBehaviour
 {
-	public string gameScene = "Game";
-	public string mainMenuScene = "Main Menu";
+    public string gameScene = "Game";
+    public string mainMenuScene = "Main Menu";
+    private bool sceneIsLoading = false;
 
-	void NewGame ()
-	{
-		Score.Reset ();
-		PlayerPrefs.DeleteKey ("Level");
-		PlayerPrefs.DeleteKey ("Time");
-		PlayerPrefs.DeleteKey ("Crashes");
-		PlayerPrefs.DeleteKey ("Timer Visible");
-		SceneManager.LoadSceneAsync (gameScene);
-	}
+    void NewGame()
+    {
+        Score.Reset();
+        PlayerPrefs.DeleteKey("Level");
+        PlayerPrefs.DeleteKey("Time");
+        PlayerPrefs.DeleteKey("Crashes");
+        PlayerPrefs.DeleteKey("Timer Visible");
+        LoadGameScene();
+    }
 
-	void Continue ()
-	{
-		Score.Reset ();
-		Score.SetCrashes (PlayerPrefs.GetInt ("Crashes"));
-		Score.SetTime (PlayerPrefs.GetFloat ("Time"));
-		SceneManager.LoadSceneAsync (gameScene);
-	}
+    void Continue()
+    {
+        Score.Reset();
+        Score.SetCrashes(PlayerPrefs.GetInt("Crashes"));
+        Score.SetTime(PlayerPrefs.GetFloat("Time"));
+        LoadGameScene();
+    }
 
-	void Update ()
-	{
-		if (Input.GetKeyDown (KeyCode.Escape)) {
-			SceneManager.LoadSceneAsync (mainMenuScene);
-		}
-	}
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadSceneAsync(mainMenuScene);
+        }
+    }
+
+    void LoadGameScene()
+    {
+        if (!sceneIsLoading)
+        {
+            sceneIsLoading = true;
+            SceneManager.LoadSceneAsync(gameScene);
+        }
+    }
 }
--- Assets/Scripts/Menu/MainMenuController.cs
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -4,50 +4,61 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-	public int state = 0;
-	public string gameSceneName;
-	public string loadSavedGameSceneName;
-	public float musicStartDelay = 1;
+    public int state = 0;
+    public string gameSceneName;
+    public string loadSavedGameSceneName;
+    public float musicStartDelay = 1;
+    private bool sceneIsLoading = false;
 
-	void Start ()
-	{
-		UnityEngine.Cursor.visible = true;
-		GameObject.Find ("Player").SendMessage ("Go");
-		GameObject.Find ("Player").SendMessage ("Pause");
-		GameObject.Find ("Music").SendMessage ("Stop");
-		Invoke ("StartMenuMusic", musicStartDelay);
-	}
+    void Start()
+    {
+        UnityEngine.Cursor.visible = true;
+        GameObject.Find("Player").SendMessage("Go");
+        GameObject.Find("Player").SendMessage("Pause");
+        GameObject.Find("Music").SendMessage("Stop");
+        Invoke("StartMenuMusic", musicStartDelay);
+    }
 
-	void StartMenuMusic ()
-	{
-		GameObject.Find ("Music").SendMessage ("PlayMenuMusic");
-	}
+    void StartMenuMusic()
+    {
+        GameObject.Find("Music").SendMessage("PlayMenuMusic");
+    }
 
-	void NextScene ()
-	{
-		if (PlayerPrefs.HasKey ("Level")) {
-			SceneManager.LoadSceneAsync (loadSavedGameSceneName);
-		} else {
-			Score.Reset ();
-			SceneManager.LoadSceneAsync (gameSceneName);
-		}
-	}
+    void NextScene()
+    {
+        if (!sceneIsLoading)
+        {
+            if (PlayerPrefs.HasKey("Level"))
+            {
+                SceneManager.LoadSceneAsync(loadSavedGameSceneName);
+            }
+            else
+            {
+                Score.Reset();
+                SceneManager.LoadSceneAsync(gameSceneName);
+            }
+            sceneIsLoading = true;
+        }
+    }
 
-	void Advance ()
-	{
-		state++;
-	}
+    void Advance()
+    {
+        state++;
+    }
 
-	void Update ()
-	{
-		if ((Input.GetButtonDown ("Jump") || Input.GetMouseButtonDown (0) || Input.GetKeyDown(KeyCode.Return)) && state >= 4) {
-			NextScene ();
-		}
-		if (Input.GetKeyDown (KeyCode.Escape)) {
-			Application.Quit ();
-		}
-		if (Input.GetKeyDown (KeyCode.R)) {
-			PlayerPrefs.DeleteAll ();
-		}
-	}
+    void Update()
+    {
+        if ((Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Return)) && state >= 4)
+        {
+            NextScene();
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            PlayerPrefs.DeleteAll();
+        }
+    }
 }
