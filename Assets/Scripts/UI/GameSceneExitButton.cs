568eaf7314e1f40b178d5c7ac829c29ac0b72e2a
41407 <jimi.welling@cs.helsinki.fi>
2016-06-03 10:48:56 +0300

    Add exit button to game scene

---
+++ Assets/Scripts/UI/GameSceneExitButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSceneExitButton : MonoBehaviour
+{
+
+    public Bounds activationZone;
+
+    void Start()
+    {
+        activationZone.center = transform.position;
+    }
+
+    void Update()
+    {
+        if (activationZone.Contains(Camera.main.ScreenToWorldPoint(Input.mousePosition)) && Input.GetMouseButtonUp(0))
+        {
+            GameObject.Find("Scene Controller").SendMessage("ExitGame");
+        }
+    }
+}
