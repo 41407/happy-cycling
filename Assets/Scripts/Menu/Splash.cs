a2ea31dfd4b562a5646054ffd115e43fbb52134a
41407 <jimi.welling@cs.helsinki.fi>
2016-06-03 10:55:59 +0300

    Fix splash script to react on buttonUps instead of downs

--- Assets/Scripts/Menu/Splash.cs
+++ Assets/Scripts/Menu/Splash.cs
@@ -3,40 +3,46 @@ using System.Collections;
 
 public class Splash : MonoBehaviour
 {
-	private Vector2 targetPosition;
-	public float startTranslateTime = 0;
-	public Vector2 finalPosition;
-	public float finishTime = 2;
+    private Vector2 targetPosition;
+    public float startTranslateTime = 0;
+    public Vector2 finalPosition;
+    public float finishTime = 2;
 
-	void Start ()
-	{
-		Invoke ("StartTranslate", startTranslateTime);
-		targetPosition = transform.position;
-	}
-	
-	void StartTranslate ()
-	{
-		StartTranslate (finishTime);
-	}
-	
-	void StartTranslate (float finish)
-	{
-		targetPosition = finalPosition;
-		Invoke ("Finish", finish);
-	}
+    void Start()
+    {
+        Invoke("StartTranslate", startTranslateTime);
+        targetPosition = transform.position;
+    }
 
-	void Finish ()
-	{
-		transform.position = targetPosition;
-		SendMessageUpwards ("Advance", SendMessageOptions.DontRequireReceiver);
-		Destroy (this);
-	}
+    void StartTranslate()
+    {
+        StartTranslate(finishTime);
+    }
 
-	void Update ()
-	{
-		if (Input.anyKeyDown) {
-			StartTranslate (0);
-		}
-		transform.position = Vector2.Lerp (transform.position, targetPosition, 0.1f);
-	}
+    void StartTranslate(float finish)
+    {
+        targetPosition = finalPosition;
+        Invoke("Finish", finish);
+    }
+
+    void Finish()
+    {
+        transform.position = targetPosition;
+        SendMessageUpwards("Advance", SendMessageOptions.DontRequireReceiver);
+        Destroy(this);
+    }
+
+    void Update()
+    {
+        if (AdvanceKeyPressed())
+        {
+            StartTranslate(0);
+        }
+        transform.position = Vector2.Lerp(transform.position, targetPosition, 0.1f);
+    }
+
+    bool AdvanceKeyPressed()
+    {
+        return Input.GetButtonUp("Jump") || Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.Return);
+    }
 }
