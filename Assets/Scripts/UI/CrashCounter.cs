ee61bea949159d6c3daefb8d43f3b2aa0adcf087
41407 <jimi.welling@cs.helsinki.fi>
2016-06-04 19:28:46 +0300

    Hide labels when counter is zero

--- Assets/Scripts/UI/CrashCounter.cs
+++ Assets/Scripts/UI/CrashCounter.cs
@@ -4,29 +4,36 @@ using System.Collections;
 
 public class CrashCounter : MonoBehaviour
 {
-	private Text t;
-	public float refreshRate = 0.16f;
-	public bool emptyIfZero = true;
-	public int current;
-	
-	void Awake ()
-	{
-		t = GetComponent<Text> ();
-		InvokeRepeating ("UpdateText", 0, refreshRate);
-		current = Score.GetCrashes ();
-	}
-	
-	void UpdateText ()
-	{
-		int number = Score.GetCrashes ();
-		if (number > current) {
-			SendMessage ("Highlight", SendMessageOptions.DontRequireReceiver);
-		}
-		current = number;
-		if (current > 0) {
-			t.text = "" + current;
-		} else {
-			t.text = "";
-		}
-	}
+    private Text t;
+    public GameObject label;
+    public float refreshRate = 0.16f;
+    public bool emptyIfZero = true;
+    public int current;
+
+    void Awake()
+    {
+        t = GetComponent<Text>();
+        InvokeRepeating("UpdateText", 0, refreshRate);
+        current = Score.GetCrashes();
+    }
+
+    void UpdateText()
+    {
+        int number = Score.GetCrashes();
+        if (number > current)
+        {
+            SendMessage("Highlight", SendMessageOptions.DontRequireReceiver);
+        }
+        current = number;
+        if (current > 0)
+        {
+            t.text = "" + current;
+            label.SetActive(true);
+        }
+        else
+        {
+            t.text = "";
+            label.SetActive(false);
+        }
+    }
 }
--- Assets/Scripts/UI/PlayerPrefCounter.cs
+++ Assets/Scripts/UI/PlayerPrefCounter.cs
@@ -4,30 +4,37 @@ using System.Collections;
 
 public class PlayerPrefCounter : MonoBehaviour
 {
-	private Text t;
-	public float refreshRate = 1;
-	public string playerPrefsKey;
-	public bool emptyIfZero = true;
-	public int current;
+    private Text t;
+    public GameObject label;
+    public float refreshRate = 1;
+    public string playerPrefsKey;
+    public bool emptyIfZero = true;
+    public int current;
 
-	void Awake ()
-	{
-		t = GetComponent<Text> ();
-		InvokeRepeating ("UpdateText", 0, refreshRate);
-		current = PlayerPrefs.GetInt (playerPrefsKey);
-	}
+    void Awake()
+    {
+        t = GetComponent<Text>();
+        InvokeRepeating("UpdateText", 0, refreshRate);
+        current = PlayerPrefs.GetInt(playerPrefsKey);
+    }
 
-	void UpdateText ()
-	{
-		int number = PlayerPrefs.GetInt (playerPrefsKey);
-		if (number > current) {
-			SendMessage ("Highlight", SendMessageOptions.DontRequireReceiver);
-		}
-		current = number;
-		if (current > 0) {
-			t.text = "" + current;
-		} else {
-			t.text = "";
-		}
-	}
+    void UpdateText()
+    {
+        int number = PlayerPrefs.GetInt(playerPrefsKey);
+        if (number > current)
+        {
+            SendMessage("Highlight", SendMessageOptions.DontRequireReceiver);
+        }
+        current = number;
+        if (current > 0)
+        {
+            t.text = "" + current;
+            label.SetActive(true);
+        }
+        else
+        {
+            t.text = "";
+            label.SetActive(false);
+        }
+    }
 }
