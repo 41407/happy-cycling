b2bfa5491d91a39b2414b8a77e90946105c1ba5c
41407 <jimi.welling@kuuasema.com>
2017-11-24 09:15:16 +0200

    Refactor HUD modes

---
+++ Assets/HUDController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class HUDController : MonoBehaviour
+{
+
+    private static HUDController instance;
+    private Animator anim;
+    public enum State
+    {
+        Gameplay,
+        Cutscene,
+    }
+
+    public static State CurrentState
+    {
+        set
+        {
+            instance.anim.SetTrigger(value == State.Gameplay ? "Gameplay" : "Cutscene");
+        }
+    }
+
+    void Awake()
+    {
+        instance = Component.FindObjectOfType<HUDController>();
+        anim = GetComponent<Animator>();
+    }
+}
